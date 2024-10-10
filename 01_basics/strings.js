2ff45ab48fd60c6b1ca09f4b922e6802f4c01c70
ABHISHEK NEGI <111852535+negia7014@users.noreply.github.com>
2024-10-10 07:15:45 +0000

    String

---
+++ 01_basics/strings.js
@@ -0,0 +1,33 @@
+// string is an object
+// to concatenate string
+const name = "abhishek"
+const repocount = 50
+//console.log(name + repocount + " Value");
+// now-a-day we use string interpolation instead of contcatenate
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+const gameName = new String('abhishek N')
+//console.log(gameName[0]);
+//console.log(gameName.__proto__); // {}
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase()); // but original string does not change beacuse string is primitive and it store in stack memory and in stack value are copies
+
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('i'));
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4);
+console.log(anotherString)
+
+const newStringone = "  abhi    "; // this string come in input form 
+console.log(newStringone);
+console.log(newStringone.trim()); // it remove starting space and end space 
+
+const url = "https://abhi.com/abhi%20negi";
+console.log(url.replace('%20','__'));
+
+console.log(url.includes('__'));
+
+console.log(gameName.split(' ')); // it convert string into array
